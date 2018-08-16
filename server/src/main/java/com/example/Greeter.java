44fdb8e32bd95e86af8dd48fcc5998cdfc75ca59
varun.raj.kollannur <varun.raj.kollannur@BDC6-LX-4190CNQ.dir.svc.accenture.com>
2018-08-16 15:36:17 +0530

    checkstyle4

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -14,7 +14,7 @@ public class Greeter {
 
   /**
    * @param someone
-   * @return
+   * @return returns string
    */
   public final String greet(final String someone) {
     return String.format("Hello @ Accenture, %s!", someone);
--- server/src/main/java/com/example/package-info.java
+++ server/src/main/java/com/example/package-info.java
@@ -1,4 +1,4 @@
 /**
- * this is a packages
+ * this is a package name.
  */
 package com.example;
